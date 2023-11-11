e961be70f1d7ac813711acf30e9e42a480c31252
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-11-12 03:01:00 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "e38d4e2c-edd5-45ec-987e-e18f81f58e34",
+    "home_feed": [
+        {
+            "id": "301acc69-f925-4814-b04a-b0e9511013c3",
+            "artist_name": "one",
+            "home_carousel": [
+                {
+                    "id": "e27f91ab-9dbb-4a3b-b716-d59c838f4f22",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/333.png"
+                },
+                {
+                    "id": "bee79fe8-5b2d-46d3-b44f-8a155350b6e9",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/210.png"
+                },
+                {
+                    "id": "bb2d3431-9db9-43b6-8302-fc519ea54100",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/203.png"
+                },
+                {
+                    "id": "97d3dbc2-0ec1-4360-8f8f-d31021a84500",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/352.png"
+                },
+                {
+                    "id": "eb5044d8-206f-42fe-81b2-10c20a24d908",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/111.png"
+                }
+            ],
+            "art_caption": "career",
+            "art_description": "How imagine measure too probably garden strategy. Choose recognize for near argue."
+        },
+        {
+            "id": "8e210abd-5f1b-45ad-9e13-b53afea4775b",
+            "artist_name": "never",
+            "home_carousel": [
+                {
+                    "id": "783473dc-8276-4d30-9a21-6ca95752dccb",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/72.png"
+                },
+                {
+                    "id": "32c4c347-eb05-4d50-a933-eaae0434a12b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/229.png"
+                },
+                {
+                    "id": "ade68202-1d49-4031-968f-d31330f330a8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/333.png"
+                },
+                {
+                    "id": "12091a6e-30c6-4778-a7c3-800f2bd77bea",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/149.png"
+                },
+                {
+                    "id": "bf3c3012-3239-43da-a366-24d2e4322a03",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/327.png"
+                }
+            ],
+            "art_caption": "begin",
+            "art_description": "Certain win ask American cultural. Behind weight room rise record Congress meeting."
+        },
+        {
+            "id": "82cc020f-d49b-4df9-bcf7-32f26c5da11f",
+            "artist_name": "dream",
+            "home_carousel": [
+                {
+                    "id": "57683e4b-1c02-4c0d-8c1e-2f56010cd6a5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/91.png"
+                },
+                {
+                    "id": "d4335efc-48bd-47fc-a109-f792416634e6",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/161.png"
+                },
+                {
+                    "id": "0b35bf83-4c5c-4953-b5f1-616e06b4aa70",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/178.png"
+                },
+                {
+                    "id": "fff2a0a6-5dea-407e-88a6-efadf35a1168",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/312.png"
+                },
+                {
+                    "id": "f607710a-c9b6-451a-bb21-9dd064a8a59d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/255.png"
+                }
+            ],
+            "art_caption": "compare",
+            "art_description": "Class receive much man hit. Can minute campaign commercial difficult add."
+        },
+        {
+            "id": "43837338-7044-4834-bb04-793c281cd44a",
+            "artist_name": "listen",
+            "home_carousel": [
+                {
+                    "id": "f313724c-df45-4c62-bc0e-6366b240f1dc",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/225.png"
+                },
+                {
+                    "id": "e73523f5-abbe-4e38-8fd5-4e60795db88d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/187.png"
+                },
+                {
+                    "id": "f1226c79-369b-45d5-867f-9725ce326c00",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/28.png"
+                },
+                {
+                    "id": "6f56e538-14ff-478f-9e3e-1fa68f3f1ce0",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/282.png"
+                },
+                {
+                    "id": "e6fb8aea-f063-4aeb-a794-1564d0869ee8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/347.png"
+                }
+            ],
+            "art_caption": "trade",
+            "art_description": "Rise like serious. Generation American scientist remember choose share. Put side office bad help."
+        },
+        {
+            "id": "79637229-506b-4989-b802-d8773e977e3b",
+            "artist_name": "PM",
+            "home_carousel": [
+                {
+                    "id": "f77e13e0-5677-4fcc-8c9d-d27b93c7ab1d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/0.png"
+                },
+                {
+                    "id": "2878c7c6-2d45-446d-be29-ae9bcca72162",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/66.png"
+                },
+                {
+                    "id": "bdcfe582-c9dd-4f82-bc3c-edbc6554965d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/135.png"
+                },
+                {
+                    "id": "88c16ee6-df86-41b3-aa3f-805f9d1933f4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/63.png"
+                },
+                {
+                    "id": "a9dec62d-7a76-4579-ba3b-d11d02c063eb",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/68.png"
+                }
+            ],
+            "art_caption": "movement",
+            "art_description": "Want minute couple win sort. Model popular reach speak need seem local total."
+        }
+    ]
+}
